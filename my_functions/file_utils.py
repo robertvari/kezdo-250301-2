f5c54462f90dffef28ed84bfb6231219a5d32f24
Robert Vari <mail.robertvari@gmail.com>
2025-03-15 17:46:51 +0100

    update

--- my_functions/file_utils.py
+++ my_functions/file_utils.py
@@ -11,9 +11,14 @@ def get_files(root_folder: str, file_list: list, filter: str=None):
 
     assert os.path.isdir(root_folder), "root_folder must be a directory."
     assert isinstance(file_list, list), "file_list must be a list."
-    assert isinstance(filter, str), "filter must be of type string."
 
+    # collect all files and folders in root_folder
+    folder_content = os.listdir(root_folder)
+
+    # create a container for subfolders
+    subfolders = []
+    
 
 if __name__ == "__main__":
     file_list = []
-    get_files(r"D:\Work\PythonSuli\kezdo-250301\alapok_2", file_list, filter=".py")
+    get_files(r"D:\Work\PythonSuli\kezdo-250301\alapok_2", file_list)
