9a9b2fc1dfc82e98789ca506fc88b11c6a811dc7
Robert Vari <mail.robertvari@gmail.com>
2025-03-15 17:09:58 +0100

    update

---
+++ 15_libraries.py
@@ -0,0 +1,7 @@
+from my_functions.math_functions import add_numbers, multiply_numbers, divide_numbers
+# from my_functions import math_functions as mf
+from my_functions.important_variables import *
+
+print(SERVER_ADDRESS)
+print(PASSWORD)
+print(USER)
---
+++ my_functions/important_variables.py
@@ -0,0 +1,3 @@
+SERVER_ADDRESS = "www.google.com"
+PASSWORD = "testpas123"
+USER = "DBuser"
