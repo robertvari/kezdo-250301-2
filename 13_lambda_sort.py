78f30d72a46568addf42a3c8e37114020485e854
Robert Vari <mail.robertvari@gmail.com>
2025-03-15 16:27:11 +0100

    update

--- 13_lambda_sort.py
+++ 13_lambda_sort.py
@@ -1,1 +1,7 @@
 name_list = ['Olivia Robinson', 'Ella Lewis', 'Isabella Rodriguez', 'Sophia White', 'Mason Taylor', 'Mason Thomas', 'Alexander Taylor', 'Elijah Thompson', 'Sophia Harris', 'Mason Taylor', 'Olivia Clark', 'Emma Taylor', 'Elijah Lewis', 'Charlotte Walker', 'Mia Robinson', 'Isabella Lee', 'Olivia Brown', 'Alexander Smith', 'Noah Hall', 'James Brown']
+
+def split_name(name):
+    result = name.split()[-1]
+    return result
+
+print(sorted(    name_list, key=lambda name: name.split()[-1]  ))
