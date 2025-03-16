d015cf2ab2c743a2f56f4b25132073419fba7040
Robert Vari <mail.robertvari@gmail.com>
2025-03-16 15:35:12 +0100

    update

---
+++ 21_thread_pool.py
@@ -0,0 +1,27 @@
+import time, random, threading, queue
+
+file_list = [
+    "image_0001.jpg",
+    "image_0002.jpg",
+    "image_0003.jpg",
+    "image_0004.jpg",
+    "image_0005.jpg",
+    "image_0006.jpg",
+    "image_0007.jpg",
+    "image_0008.jpg",
+    "image_0009.jpg",
+    "image_0010.jpg"
+]
+
+# onvert our list to a queue
+job_queue = queue.Queue()
+for i in file_list:
+    job_queue.put(i)
+
+def worker():
+    while not job_queue.empty():
+        file = job_queue.get()
+        print(f"Converting {file}...")
+        time.sleep(random.randint(6, 30))
+        print(f"Convert {file} finished!")
+        job_queue.task_done()
