65cef97a94fd872c4e4b26ab8ba603fd54bb6d76
Robert Vari <mail.robertvari@gmail.com>
2025-03-16 14:47:10 +0100

    update

--- 18_decorators.py
+++ 18_decorators.py
@@ -11,16 +11,19 @@ def my_timer(func):
     
     return wrapper
 
+@my_timer
 def worker1():
     print("Worker 1 started...")
     time.sleep(random.randint(1, 10))
     print("Worker 1 finished.")
 
+@my_timer
 def worker2():
     print("Worker 2 started...")
     time.sleep(random.randint(1, 10))
     print("Worker 2 finished.")
 
+@my_timer
 def worker3():
     print("Worker 3 started...")
     time.sleep(random.randint(1, 10))
