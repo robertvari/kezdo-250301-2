a4bea1bfc34a3cb7abb8f3017b24609993fdbd01
Robert Vari <mail.robertvari@gmail.com>
2025-03-16 14:37:28 +0100

    update

---
+++ 18_decorators.py
@@ -0,0 +1,22 @@
+import time, random
+
+
+def worker1():
+    print("Worker 1 started...")
+    time.sleep(random.randint(1, 10))
+    print("Worker 1 finished.")
+
+def worker2():
+    print("Worker 2 started...")
+    time.sleep(random.randint(1, 10))
+    print("Worker 2 finished.")
+
+def worker3():
+    print("Worker 3 started...")
+    time.sleep(random.randint(1, 10))
+    print("Worker 3 finished.")
+
+
+worker1()
+worker2()
+worker3()
