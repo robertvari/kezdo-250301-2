cb03760da8934b338c7da7bc936835ad98f21db2
Robert Vari <mail.robertvari@gmail.com>
2025-03-16 17:54:39 +0100

    update

--- tmdb_connect_v2.py
+++ tmdb_connect_v2.py
@@ -10,21 +10,35 @@ MOVIE_DATA_CACHE = os.path.join(IMAGE_LOCAL_CACHE, "movie_data.json")
 
 def main():
     movie_list = get_popular_movies()
-    download_posters(movie_list)
-    download_backdrops(movie_list)
+    download_images(movie_list)
     save_movie_data(movie_list)
 
 def get_popular_movies():
-    pass
+    movies = tmdb.Movies()
+    popular_movies = movies.popular(page=1)
+    return popular_movies["results"]
 
-def download_posters(movie_list):
-    pass
+def download_images(movie_list):
+    for movie in movie_list:
+        if not movie["poster_path"]:
+            continue
+        image_downloader(f"{POSTER_ROOT}{movie['poster_path']}")
 
-def download_backdrops(movie_list):
-    pass
+        if not movie["backdrop_path"]:
+            continue
+
+        image_downloader(f"{BACKDROP_ROOT}{movie['backdrop_path']}")
 
 def image_downloader(image_url):
-    pass
+    image_file_path = f"{IMAGE_LOCAL_CACHE}/{image_url.split('/')[-1]}"
+    if os.path.exists(image_file_path):
+        return
+    
+    print(f"Downloading {image_url}...")
+    image_data = requests.get(image_url).content
+
+    with open(image_file_path, "wb") as file:
+        file.write(image_data)
 
 def save_movie_data(movie_list):
     pass
